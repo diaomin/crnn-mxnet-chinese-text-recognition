6a929965fb181aeeb61c58fecaba045d02f98b6a
breezedeus <breezedeus@163.com>
2019-04-07 14:46:42 +0800

    bump ver

--- cnocr/utils.py
+++ cnocr/utils.py
@@ -1,3 +1,20 @@
+# coding: utf-8
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 import os
 import platform
 import zipfile
@@ -60,7 +77,7 @@ def get_model_file(root=data_dir()):
 def read_charset(charset_fp):
     alphabet = []
     # 第0个元素是预留id，在CTC中用来分割字符。它不对应有意义的字符
-    with open(charset_fp,encoding='utf-8') as fp:
+    with open(charset_fp, encoding='utf-8') as fp:
         for line in fp:
             alphabet.append(line.rstrip('\n'))
     # print('Alphabet size: %d' % len(alphabet))
