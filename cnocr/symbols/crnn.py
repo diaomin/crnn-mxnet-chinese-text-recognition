d0fdb6574d98af06b6923e1e4f40fcc6b911a856
breezedeus <breezedeus@163.com>
2020-03-12 23:22:04 +0800

    restore the original full model

--- cnocr/symbols/crnn.py
+++ cnocr/symbols/crnn.py
@@ -112,6 +112,63 @@ def bottle_conv(i, input_data, kernel_size, layer_size, padding_size, bn=True):
     return layer
 
 
+def crnn_lstm_full(hp):
+    # input
+    data = mx.sym.Variable('data')
+    label = mx.sym.Variable('label')
+    # data = mx.sym.Variable('data', shape=(128, 1, 32, 100))
+    # label = mx.sym.Variable('label', shape=(128, 4))
+
+    kernel_size = [(3, 3), (3, 3), (3, 3), (3, 3), (3, 3), (3, 3)]
+    padding_size = [(1, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1)]
+    layer_size = [min(32*2**(i+1), 512) for i in range(len(kernel_size))]
+
+    def convRelu(i, input_data, bn=True):
+        layer = mx.symbol.Convolution(name='conv-%d' % i, data=input_data, kernel=kernel_size[i], pad=padding_size[i],
+                                      num_filter=layer_size[i])
+        if bn:
+            layer = mx.sym.BatchNorm(data=layer, name='batchnorm-%d' % i)
+        layer = mx.sym.LeakyReLU(data=layer,name='leakyrelu-%d' % i)
+        layer = mx.symbol.Convolution(name='conv-%d-1x1' % i, data=layer, kernel=(1, 1), pad=(0, 0),
+                                      num_filter=layer_size[i])
+        if bn:
+            layer = mx.sym.BatchNorm(data=layer, name='batchnorm-%d-1x1' % i)
+        layer = mx.sym.LeakyReLU(data=layer, name='leakyrelu-%d-1x1' % i)
+        return layer
+
+    net = convRelu(0, data)  # bz x f x 32 x 280
+    # print('0', net.infer_shape()[1])
+    max = mx.sym.Pooling(data=net, name='pool-0_m', pool_type='max', kernel=(2, 2), stride=(2, 2))
+    avg = mx.sym.Pooling(data=net, name='pool-0_a', pool_type='avg', kernel=(2, 2), stride=(2, 2))
+    net = max - avg  # 8 x 70
+    net = convRelu(1, net)
+    # print('2', net.infer_shape()[1])
+    net = mx.sym.Pooling(data=net, name='pool-1', pool_type='max', kernel=(2, 2), stride=(2, 2)) # res: bz x f x 8 x 70
+    # print('3', net.infer_shape()[1])
+    net = convRelu(2, net, True)
+    net = convRelu(3, net)
+    net = mx.sym.Pooling(data=net, name='pool-2', pool_type='max', kernel=(2, 2), stride=(2, 2)) # res: bz x f x 4 x 35
+    # print('4', net.infer_shape()[1])
+    net = convRelu(4, net, True)
+    net = convRelu(5, net)
+    net = mx.symbol.Pooling(data=net, kernel=(4, 1), pool_type='avg', name='pool1') # res: bz x f x 1 x 35
+    # print('5', net.infer_shape()[1])
+
+    if hp.dropout > 0:
+        net = mx.symbol.Dropout(data=net, p=hp.dropout)
+
+    hidden_concat = lstm2(net, num_lstm_layer=hp.num_lstm_layer, num_hidden=hp.num_hidden)
+    # import pdb; pdb.set_trace()
+
+    # mx.sym.transpose(net, [])
+    pred = mx.sym.FullyConnected(data=hidden_concat, num_hidden=hp.num_classes, name='pred_fc') # (bz x 35) x num_classes
+    # print('pred', pred.infer_shape()[1])
+
+    if hp.loss_type:
+        # Training mode, add loss
+        return add_ctc_loss(pred, hp.seq_length, hp.num_label, hp.loss_type)
+
+
 def crnn_lstm(hp):
 
     # input
