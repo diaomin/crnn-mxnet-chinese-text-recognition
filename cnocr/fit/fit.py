65f55c9745109b5c818dfb0e7f2751d798b9a066
breezedeus <breezedeus@163.com>
2020-03-27 09:54:51 +0800

    try const lr instead of lr_scheduler

--- cnocr/fit/fit.py
+++ cnocr/fit/fit.py
@@ -16,10 +16,10 @@ def _load_model(args):
 
 
 def fit(network, data_train, data_val, metrics, args, hp, data_names=None):
-    if args.gpu:
+    if args.gpu > 0:
         contexts = [mx.context.gpu(i) for i in range(args.gpu)]
     else:
-        contexts = [mx.context.cpu(i) for i in range(args.cpu)]
+        contexts = [mx.context.cpu()]
 
     sym, arg_params, aux_params = _load_model(args)
     if sym is not None:
@@ -54,22 +54,24 @@ def fit(network, data_train, data_val, metrics, args, hp, data_names=None):
     # f = module.get_outputs()
     # import pdb; pdb.set_trace()
 
-    lr_scheduler = PolyScheduler(base_lr=hp.learning_rate, final_lr=hp.learning_rate * 0.01, max_update=hp.num_epoch // 2, pwr=2)
+    begin_epoch = args.load_epoch if args.load_epoch else 0
+    num_epoch = hp.num_epoch + begin_epoch
+    lr_scheduler = PolyScheduler(base_lr=hp.learning_rate, final_lr=hp.learning_rate * 0.01,
+                                 warmup_steps=begin_epoch, warmup_begin_lr=0, warmup_mode='linear',
+                                 max_update=hp.num_epoch // 2 + begin_epoch, pwr=2)
 
-    optimizer_params = {#'learning_rate': hp.learning_rate,
-                        'lr_scheduler': lr_scheduler,
+    optimizer_params = {'learning_rate': hp.learning_rate,
+                        #'lr_scheduler': lr_scheduler,
                         # 'momentum': hp.momentum,
                         # 'wd': 0.00001,
                         }
-    begin_epoch = args.load_epoch if args.load_epoch else 0
-    num_epoch = hp.num_epoch + begin_epoch
     module.fit(train_data=data_train,
                eval_data=data_val,
                begin_epoch=begin_epoch,
                num_epoch=num_epoch,
                # use metrics.accuracy or metrics.accuracy_lcs
                eval_metric=mx.metric.np(metrics.accuracy, allow_extra_outputs=True),
-               optimizer='AdaDelta',
+               optimizer=hp.optimizer,
                optimizer_params=optimizer_params,
                initializer=mx.init.Xavier(factor_type="in", magnitude=2.34),
                arg_params=arg_params,
