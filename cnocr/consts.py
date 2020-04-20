fd05de6181cb6166868f2244a80c322924eb2421
breezedeus <breezedeus@163.com>
2020-04-20 18:17:03 +0800

    add urls

--- cnocr/consts.py
+++ cnocr/consts.py
@@ -1,17 +1,23 @@
 # coding: utf-8
+import os
 import string
+from .__version__ import __version__
 
 
 EMB_MODEL_TYPES = ['conv', 'conv-lite', 'densenet', 'densenet-lite']
 SEQ_MODEL_TYPES = ['lstm', 'gru', 'fc']
 
+root_url = (
+    'https://raw.githubusercontent.com/breezedeus/cnocr-models/master/models/%s'
+    % __version__
+)
 # name: (epochs, url)
 AVAILABLE_MODELS = {
-    'conv-lstm': (50, ),
-    'conv-lite-lstm': (45, ),
-    'conv-lite-fc': (27, ),
-    'densenet-lite-lstm': (45, ),
-    'densenet-lite-fc': (32, ),
+    'conv-lstm': (50, root_url + '/conv-lstm-0050.zip'),
+    'conv-lite-lstm': (45, root_url + '/conv-lite-lstm-0045.zip'),
+    'conv-lite-fc': (27, root_url + '/conv-lite-fc-0027.zip'),
+    'densenet-lite-lstm': (42, root_url + '/densenet-lite-lstm-0042.zip'),
+    'densenet-lite-fc': (32, root_url + '/densenet-lite-fc.zip'),
 }
 
 # 候选字符集合
