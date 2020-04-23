4f58642d9231b0dafc7c713109088918327a3fc5
breezedeus <breezedeus@163.com>
2020-04-23 11:08:22 +0800

    replace f-print

--- cnocr/symbols/densenet.py
+++ cnocr/symbols/densenet.py
@@ -1,3 +1,20 @@
+# coding: utf-8
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 """From DenseNet in Gluon.
   from gluoncv.model_zoo.densenet import DenseNet
 """
@@ -16,7 +33,7 @@ def cal_num_params(net):
     for p in params:
         logger.info(p)
     total = sum([np.prod(p.shape) for p in params])
-    logger.info(f'total params: {total}')
+    logger.info('total params: %d' % total)
     return total
 
 
